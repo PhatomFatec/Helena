982402d69ca360068f4a38e1250e4b35ce5f3b15
Gabriel Santos <48994698+gabrieljssantos@users.noreply.github.com>
2021-04-17 13:05:19 -0300

    Alterações nas Telas das Funções
    Alterações nas Telas das Funções, adicionando layout e informações

--- src/pages/TelasFuncoes/traduzir.js
+++ src/pages/TelasFuncoes/traduzir.js
@@ -1,12 +1,63 @@
-// Comando para criar a tela : rnfc
-
 import React from 'react';
-import { View, Text } from 'react-native';
-
-// import { Container } from './styles';
+import {AppRegistry, StyleSheet, Text, TouchableOpacity, ScrollView, View, Image, Button, SafeAreaView} from 'react-native';
+import ImagemMulherSorrindo from '../../../assets/MulherSorrindo2.png';
 
 const Traduzir = () => {
-  return <View><Text>Traduzir</Text></View>;
+  return (
+
+          <SafeAreaView style={styles.container}>
+            <SafeAreaView style={styles.slide}>
+              <Image
+                source={ImagemMulherSorrindo}              
+                style={styles.ImagemMulherSorrindo}
+              />
+              <Text style={styles.title}>Informações Funções </Text>
+            </SafeAreaView>
+          </SafeAreaView>
+
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    /* backgroundColor: '#F0FF', */
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  slide: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    /* alignItems: 'flex-end', */
+    marginTop: 220,
+    width: 360,
+    height: 550,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 20,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 28,
+    color: '#000000',
+    marginTop: 32,
+    marginBottom:50,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 35,
+    fontWeight:'bold',
+    color: '#A020F0',
+    marginTop: 32,
+    marginLeft: 16,
+    marginRight: 16,
+  },
+  ImagemMulherSorrindo: {
+    height: 180,
+    width: 310,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+
 
 export default Traduzir;
